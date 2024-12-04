4b94a08a76f14a6166cd4d16d40adc177705a847
Arooran <arooransivakarunai@gmail.com>
2024-12-03 21:53:47 -0500

    commit1

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="./vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -58,7 +58,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="./file.svg"
             alt="File icon"
             width={16}
             height={16}
