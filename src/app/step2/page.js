8fc27d9c782713384a7555460079f81c1bdf4c87
Arooran <arooransivakarunai@gmail.com>
2024-12-05 23:07:34 -0500

    COMMIT6

--- src/app/step2/page.js
+++ src/app/step2/page.js
@@ -8,9 +8,6 @@ export default function Step1() {
       content={
         <>
           Once you have created your next app, go to your next.config file, and add the given line of code to the table. Also rember to use relative paths during development, as this will affect when we host this on Github Pages.
-          <pre className="bg-gray-200 p-4 rounded-md shadow-inner text-sm">
-            output: 'export',<br></br>
-          </pre>
           <p>
             This code can be found <a href="https://nextjs.org/docs/pages/building-your-application/deploying/static-exports" className="text-blue-600 underline">on this doc page!</a>
           </p>
